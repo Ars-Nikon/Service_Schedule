d8d18bd1d9ac37de8bc61795ff7f9f3e2aae5f7b
Ars-Nikon <80975576+Ars-Nikon@users.noreply.github.com>
2022-04-15 18:02:30 +0300

    Добавьте файлы проекта.

---
+++ Service_Schedule/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Service_Schedule.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
